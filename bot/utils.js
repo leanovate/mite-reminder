0e3a2c849ef159269e619ff4509572da0ff39a8b
Moritz Rumpf <moritz.rumpf@gmail.com>
2019-02-22 08:52:04 +0100

    Catch send error
    to resolve unhandled promise rejection warnings when the user was not known to slack.

--- bot/utils.js
+++ bot/utils.js
@@ -1,6 +1,10 @@
 const send = async (context, message) => {
-    const { channel: { id: channelID } } = await context.bot.openIm(context.user);
-    await context.bot.postMessage(channelID, message)
+    try {
+        const { channel: { id: channelID } } = await context.bot.openIm(context.user);
+        await context.bot.postMessage(channelID, message)
+    } catch (err) {
+        console.log("Failed to send message to user: ", err)
+    }
 }
 
 module.exports = {
