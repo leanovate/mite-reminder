80e1c226d22363e072cfdb3154b56ea126f72110
Moritz Rumpf <moritz.rumpf@gmail.com>
2019-05-17 11:07:27 +0200

    Use isTimeEnteredOnDay function
    so that times with 0 hours are registered as uncompleted, too

--- mite/mite-api.js
+++ mite/mite-api.js
@@ -1,5 +1,5 @@
 const miteApi = require("mite-api")
-const { getDatesBetween, isWeekend } = require("./time")
+const { getDatesBetween, isWeekend, isTimeEnteredOnDay } = require("./time")
 
 const createMiteApi = apiKey => miteApi({
     account: 'leanovate',
@@ -17,12 +17,11 @@ getTimeEntries = (mite, userId, from, to) =>
             reject(result)
             return
         }
-        const times = result.map(entry => entry.time_entry.date_at)
         const datesToCheck = getDatesBetween(from, to)
         resolve(datesToCheck
             .filter(date => !date.isHoliday())
             .filter(date => !isWeekend(date))
-            .filter(date => !times.includes(date.format("YYYY-MM-DD")))
+            .filter(date => !isTimeEnteredOnDay(result, date))
         )
     }))
 
