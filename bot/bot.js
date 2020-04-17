70b353b154a76f5af100cd4f9adbaa33e0739cf8
Moritz Rumpf <moritz.rumpf@gmail.com>
2020-04-17 08:14:55 +0200

    Use different help text if there is no admin key

--- bot/bot.js
+++ bot/bot.js
@@ -9,11 +9,17 @@ const { getConfig } = require("./config")
 const env = getConfig()
 const { timezone } = env
 
-const helpText = `
+const helpTextAdmin = `
 Use \`register\` to receive mite reminders in the future.
 Use \`check\` to for missing time entries. Holidays and weekends are automatically excluded.
 Use \`unregister\` to undo your registration.
 `
+const helpTextNoAdmin = `
+Use \`register <MITE_API_KEY>\` to receive mite reminders in the future. You can find your api key here: https://leanovate.mite.yo.lk/myself
+Use \`check\` to for missing time entries. Holidays and weekends are automatically excluded.
+Use \`unregister\` to undo your registration.
+`
+const helpText = env.useMiteAdminKey ? helpTextAdmin : helpTextNoAdmin
 
 const commands = context => ({
     register: ({ miteApiKey }) => registerUser(context, miteApiKey),
