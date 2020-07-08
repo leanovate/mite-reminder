2e8be3dda1cc10f1e0674dd126ad41898f283cb9
Kai <kai@noffke.io>
2020-07-08 09:23:26 +0200

    Removed an interface that is not needed any longer

--- src/mite/holidays.ts
+++ src/mite/holidays.ts
@@ -3,10 +3,6 @@ import Holidays from 'date-holidays'
 
 const hd = new Holidays("DE","BE")
 
-export interface MomentHoliday extends Moment {
-  isHoliday(): boolean
-}
-
 export const isHoliday = (moment: Moment): boolean => {
   const holiday = hd.isHoliday(moment.toDate())
   return holiday && holiday.type === "public"
