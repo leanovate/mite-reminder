122cd3817094995d916d9945bbb9087948b1c4fc
Kai <kai@noffke.io>
2020-07-08 11:18:52 +0200

    Implemented and tested the userRepository.

---
+++ src/db/user-repository.ts
@@ -0,0 +1,30 @@
+import { FileHandle } from 'fs/promises'
+
+export type User = {
+    miteApiKey?: string
+}
+
+export type DB = { [slackId: string]: User }
+
+export async function registerUser(db: DB, fileHandle: FileHandle, slackId: string, miteApiKey?: string) {
+    db[slackId] = { miteApiKey }
+    await updateDatabase(db, fileHandle)
+}
+
+export async function unregisterUser(db: DB, fileHandle: FileHandle, slackId: string) {
+    if(!db[slackId]) {
+        console.log(`User with slackId ${slackId} is unknown, hence cannot be unregistered.`)
+        return
+    }
+
+    delete db[slackId]
+    await updateDatabase(db, fileHandle)
+}
+
+export function loadUser(db: DB, slackId: string): User | null {
+    return db[slackId] || null
+}
+
+function updateDatabase(db: DB, fileHandle: FileHandle): Promise<void> {
+    return fileHandle.writeFile(JSON.stringify(db), { encoding: 'utf-8' })
+}
---
+++ test/db/user-repository.test.ts
@@ -0,0 +1,69 @@
+import { BaseEncodingOptions } from 'fs'
+import { FileHandle } from 'fs/promises'
+import { registerUser, loadUser, unregisterUser } from '../../src/db/user-repository'
+
+describe('User Repository', () => {
+
+    const fileHandleMock = {
+        writeFile: jest.fn(() => { })
+    } as unknown as FileHandle
+
+    afterEach(() => {
+        jest.clearAllMocks()
+    })
+
+    it('should save information about a user', async () => {
+        const testDb = {}
+        const slackId = 'slack-id'
+        const miteApiKey = 'mite-api-key'
+        await registerUser(testDb, fileHandleMock, slackId, miteApiKey)
+
+        expect(fileHandleMock.writeFile).toBeCalledTimes(1)
+        expect(testDb).toEqual({ [slackId]: { miteApiKey } })
+        expect(fileHandleMock.writeFile).toBeCalledWith(JSON.stringify(testDb), { encoding: 'utf-8' })
+    })
+
+    it('should load information about a user', () => {
+        const slackId = 'slack-id'
+        const miteApiKey = 'mite-api-key'
+        const testDb = {
+            [slackId]: { miteApiKey }
+        }
+
+        const user = loadUser(testDb, slackId)
+        expect(user?.miteApiKey).toEqual(miteApiKey)
+    })
+
+    it('should return null if the user cannot be found', () => {
+        const testDb = {}
+
+        const user = loadUser(testDb, 'slack-id')
+        expect(user).toBeNull()
+    })
+
+    it('should remove a user from the storage', async () => {
+        const slackId = 'slack-id'
+        const miteApiKey = 'mite-api-key'
+        const testDb = {
+            [slackId]: { miteApiKey }
+        }
+
+        await unregisterUser(testDb, fileHandleMock, slackId)
+
+        expect(testDb).toEqual({})
+        expect(fileHandleMock.writeFile).toHaveBeenCalledTimes(1)
+        expect(fileHandleMock.writeFile).toBeCalledWith(JSON.stringify(testDb), { encoding: 'utf-8' })
+    })
+
+    it('should do nothing when user to be unregistered cannot be found', async () => {
+        const slackId = 'slack-id'
+        const miteApiKey = 'mite-api-key'
+        const testDb = {
+            [slackId]: { miteApiKey }
+        }
+
+        await unregisterUser(testDb, fileHandleMock, 'unknown-slack-id')
+        expect(testDb).toEqual({[slackId]: { miteApiKey }})
+        expect(fileHandleMock.writeFile).toHaveBeenCalledTimes(0)
+    })
+})
