0287ffd90a71314856bfce52766f4d28accedec2
Kai <kai@noffke.io>
2020-07-14 07:47:53 +0200

    Make it possible to pass in a mite-api.
    I guess this is needed to use a different api key than the one which is passed in through the config object.

--- src/reminder/reminder.ts
+++ src/reminder/reminder.ts
@@ -4,16 +4,17 @@ import { createMiteApi, getTimeEntries } from "../mite/mite-api-wrapper"
 import { getDatesBetween, isTimeEnteredOnDay, isWeekend } from "../mite/time"
 import { TimeEntries } from "../mite/types"
 import config from "../config"
+import { MiteApi } from "mite-api"
 
 if (!config.miteApiKey) {
     throw new Error("mite api key not set") //TODO make this a non-requirement
 }
 const miteApi = createMiteApi(config.miteApiKey)
 
-export async function getMissingTimeEntries(miteUserId: string, from: Moment, to: Moment): Promise<Moment[]> {
+export async function getMissingTimeEntries(miteUserId: string, from: Moment, to: Moment, api: MiteApi = miteApi): Promise<Moment[]> {
     let timeEntries: TimeEntries = []
     try {
-        timeEntries = await getTimeEntries(miteApi, miteUserId, from, to)
+        timeEntries = await getTimeEntries(api, miteUserId, from, to)
     }
     catch (error) {
         console.error("Failed to get time entries: ", error)
