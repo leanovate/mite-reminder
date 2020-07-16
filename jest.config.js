a3e1a263c7ee3026f70517e17c5dc252affd4d30
Moritz Rumpf <moritz.rumpf@gmail.com>
2020-07-16 10:51:49 +0200

    Make tests run with ts-jest
    so that compile errors are reported during the test run

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   transform: {'^.+\\.ts?$': 'ts-jest'},
   testEnvironment: 'node',
   testRegex: '/test/.*\\.(test|spec)?\\.ts$',
-  moduleFileExtensions: ['ts', 'js', 'json', 'node']
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  preset: 'ts-jest'
 };
