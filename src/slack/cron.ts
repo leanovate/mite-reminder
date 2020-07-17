7bf56ed78edd67fc40b977a593650f3629d07902
Moritz Rumpf <moritz.rumpf@gmail.com>
2020-07-17 15:33:15 +0200

    Run cron job for testing
    Co-authored-by: kainoffke <kainoffke@users.noreply.github.com>

--- src/slack/cron.ts
+++ src/slack/cron.ts
@@ -5,15 +5,15 @@ import { getMissingTimeEntries } from "../reminder/reminder"
 import { createMiteApi } from "../mite/mite-api-wrapper"
 import { lastWeekThursdayToThursday } from "../mite/time"
 import moment from "moment"
-import { SayFn } from "@slack/bolt"
+import { App } from "@slack/bolt"
 
 const { timezone } = config
 
-export const scheduleCronJobs = (repository: Repository, say: SayFn): void => {
-    scheduleDailyCron(repository, say)
+export const scheduleCronJobs = (app: App, repository: Repository): void => {
+    scheduleDailyCron(repository, app)
 }
 
-const scheduleDailyCron = (repository: Repository, say: SayFn) => {
+const scheduleDailyCron = (repository: Repository, app: App) => {
     cron.schedule("0 9 * * 1-5", () => {
         const users = repository.loadAllUsers()
         console.log(`Running daily cron for ${users.length} users.`)
@@ -22,17 +22,28 @@ const scheduleDailyCron = (repository: Repository, say: SayFn) => {
 
             const miteId = repository.getMiteId(user.slackId)
             if (miteId === null) {
-                say("I cannot remind you because I can't find your mite account. Please register with your mite api key from https://leanovate.mite.yo.lk/myself and send `register <YOUR_MITE_API_KEY>`.")
+                app.client.chat.postMessage({
+                    token: config.slackToken,
+                    channel: user.slackId,
+                    text: "I cannot remind you because I can't find your mite account. Please register with your mite api key from https://leanovate.mite.yo.lk/myself and send `register <YOUR_MITE_API_KEY>`."
+                })
                 return
             }
             const miteApi = createMiteApi(miteId)
             getMissingTimeEntries(miteId, start, end, miteApi)
                 .then(times => {
-                    if (times.length > 0) {
-                        say("Your time entries for the following dates are missing or contain 0 minutes:\n"
-                            + times.map(date => `https://leanovate.mite.yo.lk/#${date.format("YYYY/MM/DD")}`)
-                                .join("\n"))
-                    }
+                    // if (times.length > 0) {
+
+                    app.client.chat.postMessage({
+                        token: config.slackToken,
+                        channel: user.slackId,
+                        text: "Yay cron job ran"
+                    })
+
+                    // say("Your time entries for the following dates are missing or contain 0 minutes:\n"
+                    //     + times.map(date => `https://leanovate.mite.yo.lk/#${date.format("YYYY/MM/DD")}`)
+                    //         .join("\n"))
+                    // }
                 }).catch(e => {
                     console.log("Error when running daily cron:", e)
                     // TODO also send an error via slack?
