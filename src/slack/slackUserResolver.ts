448a1b7e035822b63db2ee40541d83e747b33f03
Moritz Rumpf <moritz.rumpf@gmail.com>
2020-07-21 09:38:03 +0200

    Catch errors in the slackUserResolver
    Not catching them will result in the user getting no feedback after the register command failed.

--- src/slack/slackUserResolver.ts
+++ src/slack/slackUserResolver.ts
@@ -3,9 +3,15 @@ import { SlackApiUser } from "./events"
 import config from "../config"
 
 export const slackUserResolver: (app: App) => (id: string) => Promise<{ email: string | undefined }> = app => async id => {
-    const apiCallResult: SlackApiUser = await app.client.users.info({
-        user: id,
-        token: config.slackToken
-    })
-    return { email: apiCallResult.user?.profile?.email }
+    try {
+
+        const apiCallResult: SlackApiUser = await app.client.users.info({
+            user: id,
+            token: config.slackToken
+        })
+        return { email: apiCallResult.user?.profile?.email }
+    } catch (e) {
+        console.warn(`Failed get the email of user '${id}' from slack because of:`, e)
+        return { email: undefined }
+    }
 }
