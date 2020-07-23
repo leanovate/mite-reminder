703f49827ef3dc532f28cdbc0c833c4c35b0dd27
Kai Noffke <kai@noffke.io>
2020-07-23 14:37:52 +0200

    Fixed Tests
    Testing the SlackUserResolver now uses TaskEithers

--- test/slack/slackUserResolver.test.ts
+++ test/slack/slackUserResolver.test.ts
@@ -1,25 +1,44 @@
 jest.mock("../../src/config", () => ({
 }))
 
-import { slackUserResolver } from "../../src/slack/slackUserResolver"
 import { App } from "@slack/bolt"
+import { Either } from "fp-ts/lib/Either"
+import { isRight, isLeft } from "fp-ts/lib/These"
+import { slackUserResolver } from "../../src/slack/slackUserResolver"
+import { UnknownAppError } from "../../src/app/errors"
 
 describe("Help", () => {
     it("should return the email", async () => {
         const email = "test@email.com"
-        const slackApp: App = { client: { users: { info: () => ({ user: { profile: { email } } }) } } as unknown } as App
+        const slackApp: App = { client: { users: { info: () => Promise.resolve({ user: { profile: { email } } }) } } as unknown } as App
 
-        const result = await slackUserResolver(slackApp)("test-slack-id")
+        const result = await slackUserResolver(slackApp)("test-slack-id")()
 
-        expect(result).toEqual({ email })
+        expect(getLeft(result)).toEqual({ email })
     })
 
-    it("should return undefined when the slack api throws", async () => {
+    it("should return an AppError when the slack api throws", async () => {
         const slackApiError = new Error("You are missing the read.users.email scope")
-        const slackApp: App = { client: { users: { info: () => { throw slackApiError } } } as unknown } as App
+        const slackApp: App = { client: { users: { info: () => Promise.reject(slackApiError) } } as unknown } as App
 
-        const result = await slackUserResolver(slackApp)("test-slack-id")
+        const result = await slackUserResolver(slackApp)("test-slack-id")()
 
-        expect(result).toEqual({ email: undefined })
+        expect(getRight(result)).toEqual(new UnknownAppError(slackApiError))
     })
 })
+
+function getLeft<T>(either: Either<unknown, T>): T {
+    if(isRight(either)) {
+        return either.right
+    }
+
+    throw either.left
+}
+
+function getRight<T>(either: Either<T, unknown>): T {
+    if(isLeft(either)) {
+        return either.left
+    }
+
+    throw either.right
+}
