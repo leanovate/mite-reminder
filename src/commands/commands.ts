2ab4a2eb88ba68e5c0fb8ccca6dbcc1f399203af
Kai Noffke <kai@noffke.io>
2020-07-23 15:22:47 +0200

    Removed unneccessary log
    Co-authored-by: Moritz Rumpf <moritz.rumpf@gmail.com>

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -33,11 +33,7 @@ export function doRegister(command: RegisterCommand, context: UserContext, email
         emailResolver(context.slackId),
         T.chain(email => getMiteIdByEmail(context.miteApi, email.email)),
         T.chain(orElseFailWith(new UserIsUnknown(context.slackId))),
-        T.chain(id => {
-            console.log("ID", id)
-            return    context.repository.registerUserWithMiteId(context.slackId, id)
-        
-        }),
+        T.chain(id => context.repository.registerUserWithMiteId(context.slackId, id))
     )
 }
 
