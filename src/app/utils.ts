6365f5005b50a56ebe461cbcefd23a1d3fb6c44e
Moritz Rumpf <moritz.rumpf@gmail.com>
2020-07-24 10:09:06 +0200

    Remove unused method

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -6,9 +6,3 @@ import { AppError } from "./errors"
 export function orElseFailWith<VALUE>(onNone: AppError): (option: Option<VALUE>) => TaskEither<AppError, VALUE> {
     return option.fold(() => T.left<AppError, VALUE>(onNone), (value: VALUE) => T.right<AppError, VALUE>(value))
 }
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const log = <VALUE>(...logMessage: any[]) => (v: VALUE): VALUE => {
-    console.log(logMessage)
-    return v
-}
