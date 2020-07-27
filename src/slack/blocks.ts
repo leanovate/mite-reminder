e05f6e37ddfa62769f7d33238c791f276ca1300d
Kai Noffke <kai@noffke.io>
2020-07-27 15:08:25 +0200

    Fixed usability TODO
    We display a nice message if the user has completed all time entries.

--- src/slack/blocks.ts
+++ src/slack/blocks.ts
@@ -1,8 +1,23 @@
 import { DividerBlock, KnownBlock, SectionBlock } from "@slack/web-api"
 import { Moment } from "moment"
 
-// TODO: Display something like "You completed all your time entries" when there are no missing times
+const emptyTimesBlock: KnownBlock = {
+    type: "section",
+    text: {
+        type: "plain_text",
+        emoji: true,
+        text: "You have completed all your time entries :ok_hand:"
+    }
+}
+
 export const missingTimeEntriesBlock = (times: Moment[]): {text: string, blocks: KnownBlock[]}  =>  {
+    if(times.length === 0) {
+        return {
+            text: "You have completed all your time entries",
+            blocks: [emptyTimesBlock]
+        }
+    }
+
     const header: SectionBlock = {
         type: "section",
         text: {
@@ -10,7 +25,7 @@ export const missingTimeEntriesBlock = (times: Moment[]): {text: string, blocks:
             "text": ":clock1: Your time entries for the following dates are _missing_ or contain _0 minutes_:"
         }
     }
-    
+
     const divider: DividerBlock = {
         type: "divider"
     }
@@ -19,7 +34,7 @@ export const missingTimeEntriesBlock = (times: Moment[]): {text: string, blocks:
         type: "section",
         text: {
             type: "mrkdwn",
-            text: times.map(time => `• <https://leanovate.mite.yo.lk/#${time.format("YYYY/MM/DD")}|*${time.format("DD.MM.YYYY")}*>`).join("\n")
+            text: times.map(time => `• <https://leanovate.mite.yo.lk/#${time.format("YYYY/MM/DD")}|*${time.format("DD.MM.YYYY")}*>`).join("\n") // TODO Replace "leanovate" with the value from config
         }
     }    
     
