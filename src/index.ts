06151328eb55ca09cfef25affb7b2e88c7fb6792
Kai Noffke <kai@noffke.io>
2020-07-28 09:04:23 +0200

    Aligned the naming of createUserRepository

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import SlackApi from "./slack/api"
-import { createRepository } from "./db/create-user-repository"
+import { createRepository } from "./db/createUserRepository"
 import { pipe } from "fp-ts/lib/function"
 import { fold } from "fp-ts/lib/TaskEither"
 
