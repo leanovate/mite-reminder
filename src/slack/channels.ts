a6abf16ce842b8fc5f40b85e5687062fea4d79ac
Moritz Rumpf <moritz.rumpf@gmail.com>
2020-09-02 17:09:43 +0200

    Add missing auth to slack client command

--- src/slack/channels.ts
+++ src/slack/channels.ts
@@ -2,11 +2,12 @@ import { App } from "@slack/bolt"
 import { taskEither as T } from "fp-ts"
 import { TaskEither } from "fp-ts/lib/TaskEither"
 import { AppError, UnknownAppError } from "../app/errors"
+import config from "../config"
 
 export const getAllUsersFromChannel = (app: App, channelName: string): TaskEither<AppError, string[]> =>
     T.tryCatch(
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        () => app.client.conversations.members({ channel: channelName }).then((result: any) => result["members"]),
+        () => app.client.conversations.members({ channel: channelName, token: config.slackToken }).then((result: any) => result["members"]),
         (e: unknown) => {
             console.warn(`Failed to get members of channel ${channelName}`, e)
             return new UnknownAppError(e)
