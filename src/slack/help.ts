befafd1c5b1b9a141b8fbe0cc0c41fc9e1b839f6
David Gontrum <david.gontrum@leanovate.de>
2020-11-10 15:41:31 +0100

    added some help text for the projects command

--- src/slack/help.ts
+++ src/slack/help.ts
@@ -3,6 +3,7 @@ import { SayFn } from "@slack/bolt"
 
 const sharedHelpText = `
 Use \`sync\` to synchronize your calendar events with mite. All events with #mite <PROJECT_ID>/<SERVICE_ID> in description within the last seven days will be added automatically.
+Use \`projects <SEARCH_TEXT>\` the Project- or Service-IDs where the name contains <SEARCH_TEXT>.
 Use \`check <CHANNEL>\` to get a list of people in channel which filled all mite events for the last week and those who didn't.
 Use \`check\` to for missing time entries. Holidays and weekends are automatically excluded.
 Use \`unregister\` to undo your registration.
